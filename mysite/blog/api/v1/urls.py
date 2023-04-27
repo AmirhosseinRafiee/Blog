10c342a59837d8443769664b4ef3076358bc9f08
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-04-27 04:46:46 +0330

    updated blog api to genericapiviews

--- mysite/blog/api/v1/urls.py
+++ mysite/blog/api/v1/urls.py
@@ -6,6 +6,8 @@ app_name = 'api-v1'
 urlpatterns = [
     # path('', views.post_list, name='post-list'),
     # path('create/', views.create_post, name='post-create'),
+    # path('', views.PostListApiView.as_view(), name='post-list'),
+    # path('post/<int:id>/', views.PostDetailApiView.as_view(), name='post-detail'),
     path('', views.PostListApiView.as_view(), name='post-list'),
-    path('post/<int:id>/', views.PostDetailApiView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', views.PostDetailApiView.as_view(), name='post-detail'),
 ]
--- mysite/blog/api/v1/views.py
+++ mysite/blog/api/v1/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.views import Response, APIView
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from ...models import Post
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
@@ -26,47 +27,68 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 #     serializer.save()
 #     return Response(serializer.data)
 
-class PostListApiView(APIView):
+# ApiViews
+# class PostListApiView(APIView):
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+
+#     def get(self, request):
+#         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+#         if not request.user.is_authenticated:
+#             posts = posts.filter(login_require=False)
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+# class PostDetailApiView(APIView):
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get(self, request, id):
+#         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+#         if not request.user.is_authenticated:
+#             posts = posts.filter(login_require=False)
+#         post = get_object_or_404(posts, pk=id)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+    
+#     def put(self, request, id):
+#         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+#         if not request.user.is_authenticated:
+#             posts = posts.filter(login_require=False)
+#         post = get_object_or_404(posts, pk=id)
+#         serializer = PostSerializer(post, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+    
+#     def delete(self, request, id):
+#         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
+#         if not request.user.is_authenticated:
+#             posts = posts.filter(login_require=False)
+#         post = get_object_or_404(posts, pk=id)
+#         post.delete()
+#         return Response({'detail': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+class PostListApiView(ListCreateAPIView):
+    serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request):
+    def get_queryset(self):
         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
-        if not request.user.is_authenticated:
+        if not self.request.user.is_authenticated:
             posts = posts.filter(login_require=False)
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        return posts
+    
+class PostDetailApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-class PostDetailApiView(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
-
-    def get(self, request, id):
+    def get_queryset(self):
         posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
-        if not request.user.is_authenticated:
+        if not self.request.user.is_authenticated:
             posts = posts.filter(login_require=False)
-        post = get_object_or_404(posts, pk=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
-        if not request.user.is_authenticated:
-            posts = posts.filter(login_require=False)
-        post = get_object_or_404(posts, pk=id)
-        serializer = PostSerializer(post, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, id):
-        posts = Post.objects.filter(status=True, published_date__lte=timezone.now())
-        if not request.user.is_authenticated:
-            posts = posts.filter(login_require=False)
-        post = get_object_or_404(posts, pk=id)
-        post.delete()
-        return Response({'detail': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+        return posts
